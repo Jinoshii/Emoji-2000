1744531ec16448a380e70282357c8b3d0084ca73
Jeremy M <Jinoshi@IT-020537.local>
2022-11-16 11:52:57 -0600

    progress with emojis

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const myEmojis = ["🚲", "🥋", "♟"]
+const myEmojis = ["🎮", "👨‍💻", "🍆"]
+
+let emojiBank = document.getElementById("emoji-element")
+
+
+for (i=0; i<myEmojis.length; i++) {
+	const emoji = document.createElement('span')
+	emoji.textContent = myEmojis[i]
+	emojiBank.append(emoji)
+}
+
+
+
+// for (item in myEmojis) {
+// 	console.log(item)
+// }
