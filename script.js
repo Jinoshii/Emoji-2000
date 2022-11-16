5fbd81b31e551b886ef5fa9dcec04c9a90c3e6a8
Jeremy M <Jinoshi@IT-020537.local>
2022-11-16 18:19:35 +0000

    code for imput and button

--- script.js
+++ script.js
@@ -2,12 +2,28 @@ const myEmojis = ["🎮", "👨‍💻", "🍆"]
 
 let emojiBank = document.getElementById("emoji-element")
 
-
-for (i=0; i<myEmojis.length; i++) {
-	const emoji = document.createElement('span')
-	emoji.textContent = myEmojis[i]
-	emojiBank.append(emoji)
+function displayEmojis(){
+	for (i=0; i<myEmojis.length; i++) {
+		const emoji = document.createElement('span')
+		emoji.textContent = myEmojis[i]
+		emojiBank.append(emoji)
+	}
 }
+
+displayEmojis()
+
+
+const pushButton = document.getElementById("push-btn")
+pushButton.addEventListener("click", function(){
+	const emojiInput = document.getElementById("emoji-input")
+	
+	if (emojiInput.value) {
+		myEmojis.push(emojiInput.value)
+		emojiInput.value = ""
+		emojiBank.innerHTML = ""
+		displayEmojis()
+	}
+})
 
 
 
